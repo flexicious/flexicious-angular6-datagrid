bcad01a0846989c7646a08428a45bbec4658fec5
SH <flexicious@gmail.com>
2019-04-30 04:14:06 -0400

    Fix for IE11

--- src/assets/libs/configuration.js
+++ src/assets/libs/configuration.js
@@ -5,3 +5,6 @@ myCompanyNameSpace.SAMPLE_CONFIGS_DETAIL_URLS = {};
 myCompanyNameSpace.SAMPLE_CONFIGS_DETAIL_BEAUTIFIED = {};
 myCompanyNameSpace.IMAGE_PATH = "http://www.htmltreegrid.com/demo/flexicious/css/images";
 flexiciousNmsp.Constants.IMAGE_PATH = "http://www.htmltreegrid.com/demo/flexicious/css/images/";
+
+var UIUtils = flexiciousNmsp.UIUtils;
+UIUtils.ieVersion = 11;
